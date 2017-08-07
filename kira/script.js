e14a9719f41c396113ef4f2f8844f278a4002bb5
Kübra Özdemir <kubra.ozdemir.92@gmail.com>
2017-08-07 16:48:16 +0300

    kira artış oranına ekleme, ana sayfa tasarımında ufak değişiklik

--- kira/script.js
+++ kira/script.js
@@ -3,23 +3,32 @@
  function buttonact(){
  var radio0= document.getElementById("artisorani").checked; 
  var radio1= document.getElementById("ozelartisorani").checked;
- var radio2= document.getElementById("kiralanan").value;
+ var radio2= document.getElementById("konutCheck").checked;
+ var radio3= document.getElementById("isyeriCheck").checked;
+ var radio4= document.getElementById("ortalama").checked;
+ var radio5= document.getElementById("tüfe").checked;
 
-
-
-
-  if(radio0 == true && radio2 == 1 ){
+  if(radio0 == true && radio2 == true && radio3 == false ){
     konutartishesapla();
 
   }
-  else if(radio0 == true && radio2 == 2 ){
+  else if(radio0 == true && radio3 == true && radio2 == false){
     isyeriartishesapla();
   }
 
-else if(radio0 == false && radio1 == true ){
+  else if(radio4 == true && radio3 == true){
+    isyeriartishesapla2();
+  }
+
+  else if (radio5 == true && radio3 == true) {
+    isyeriartishesapla3();
+  }
+
+  else if(radio0 == false && radio1 == true ){
     sum();
 
 }
+
 
 }
 
@@ -72,8 +81,8 @@ else if(radio0 == false && radio1 == true ){
        var result= parseInt(FirstNumberValue) + artis;
 
      }
-     else if (Months == '8' && Year == '2'){
-      var artis= parseInt(FirstNumberValue) * 4.77 /100;
+     else if (Months == '8' && Year == '1'){
+      var artis= parseInt(FirstNumberValue) * 10.94 /100;
        var result= parseInt(FirstNumberValue) + artis;
      }
       else if (Months == '9' && Year == '2'){
@@ -97,11 +106,83 @@ else if(radio0 == false && radio1 == true ){
        }
    }
 
+/*Yurt İçi Üretici Fiyat Endeksi (Yİ-ÜFE)*/
+
 /*iş yeri için aya göre artış hesaplama*/
    function isyeriartishesapla() {
        var FirstNumber = document.getElementById('mevcutkira').value;
        var Months = document.getElementById('aylar').value;
        var Year = document.getElementById('yillar').value;
+
+
+/*ocak*/
+   if( Months == '1' && Year == '1'){
+       var artis= parseInt(FirstNumber) * 4.3 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+
+     /*şubat*/
+     else if (Months == '2' && Year == '1'){
+      var artis= parseInt(FirstNumber) * 4.96 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+     /*mart*/
+     else if (Months == '3' && Year == '1'){
+      var artis= parseInt(FirstNumber) * 5.87 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+     /*nisan*/
+     else if (Months == '4' && Year == '1'){
+      var artis= parseInt(FirstNumber) * 6.89 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+     /*mayıs*/
+     else if (Months == '5' && Year == '1'){
+      var artis= parseInt(FirstNumber) * 8.01 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+     /*haziran*/
+     else if (Months == '6' && Year == '1'){
+      var artis= parseInt(FirstNumber) * 9.02 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+     /*temmuz*/ 
+     else if (Months == '7' && Year == '1'){
+      var artis= parseInt(FirstNumber) * 9.98 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+      else if (Months == '8' && Year == '1'){
+      var artis= parseInt(FirstNumber) * 10.94 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+      else if (Months == '9' && Year == '2'){
+      var artis= parseInt(FirstNumber) * 4.51 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+      else if (Months == '10' && Year == '2'){
+      var artis= parseInt(FirstNumber) * 4.07 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+      else if (Months == '11' && Year == '2'){
+      var artis= parseInt(FirstNumber) * 3.83 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+      else if (Months == '12' && Year == '2'){
+      var artis= parseInt(FirstNumber) * 3.93 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+       if (!isNaN(result)) {
+           document.getElementById('hesapsonuc').value = result + ' TL';
+       }
+   }
+
+/*TEFE Yİ-ÜFE Ortalama*/
+/*iş yeri için aya göre artış hesaplama*/
+   function isyeriartishesapla2() {
+       var FirstNumber = document.getElementById('mevcutkira').value;
+       var Months = document.getElementById('aylar').value;
+       var Year = document.getElementById('yillar').value;
+
 
 /*ocak*/
    if( Months == '1' && Year == '1'){
@@ -139,25 +220,94 @@ else if(radio0 == false && radio1 == true ){
       var artis= parseInt(FirstNumber) * 9.67 /100;
        var result= parseInt(FirstNumber) + artis;
      }
-      else if (Months == '8' && Year == '2'){
-      var artis= parseInt(FirstNumberValue) * 6.34 /100;
-       var result= parseInt(FirstNumberValue) + artis;
+      else if (Months == '8' && Year == '1'){
+      var artis= parseInt(FirstNumber) * 10.19 /100;
+       var result= parseInt(FirstNumber) + artis;
      }
       else if (Months == '9' && Year == '2'){
-      var artis= parseInt(FirstNumberValue) * 6.245 /100;
-       var result= parseInt(FirstNumberValue) + artis;
+      var artis= parseInt(FirstNumber) * 6.245 /100;
+       var result= parseInt(FirstNumber) + artis;
      }
       else if (Months == '10' && Year == '2'){
-      var artis= parseInt(FirstNumberValue) * 5.995 /100;
-       var result= parseInt(FirstNumberValue) + artis;
+      var artis= parseInt(FirstNumber) * 5.995 /100;
+       var result= parseInt(FirstNumber) + artis;
      }
       else if (Months == '11' && Year == '2'){
-      var artis= parseInt(FirstNumberValue) * 5.86 /100;
-       var result= parseInt(FirstNumberValue) + artis;
+      var artis= parseInt(FirstNumber) * 5.86 /100;
+       var result= parseInt(FirstNumber) + artis;
      }
       else if (Months == '12' && Year == '2'){
-      var artis= parseInt(FirstNumberValue) * 5.86 /100;
-       var result= parseInt(FirstNumberValue) + artis;
+      var artis= parseInt(FirstNumber) * 5.86 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+       if (!isNaN(result)) {
+           document.getElementById('hesapsonuc').value = result + ' TL';
+       }
+   }
+
+
+/*TÜFE ORANLARI*/
+   function isyeriartishesapla3() {
+       var FirstNumber = document.getElementById('mevcutkira').value;
+       var Months = document.getElementById('aylar').value;
+       var Year = document.getElementById('yillar').value;
+
+
+/*ocak*/
+   if( Months == '1' && Year == '1'){
+       var artis= parseInt(FirstNumber) * 6.04 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+
+     /*şubat*/
+     else if (Months == '2' && Year == '1'){
+      var artis= parseInt(FirstNumber) * 7.78 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+     /*mart*/
+     else if (Months == '3' && Year == '1'){
+      var artis= parseInt(FirstNumber) * 7.76 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+     /*nisan*/
+     else if (Months == '4' && Year == '1'){
+      var artis= parseInt(FirstNumber) * 7.88 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+     /*mayıs*/
+     else if (Months == '5' && Year == '1'){
+      var artis= parseInt(FirstNumber) * 8.66 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+     /*haziran*/
+     else if (Months == '6' && Year == '1'){
+      var artis= parseInt(FirstNumber) * 9.09 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+     /*temmuz*/ 
+     else if (Months == '7' && Year == '1'){
+      var artis= parseInt(FirstNumber) * 9.36 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+      else if (Months == '8' && Year == '1'){
+      var artis= parseInt(FirstNumber) * 9.44 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+      else if (Months == '9' && Year == '2'){
+      var artis= parseInt(FirstNumber) * 7.79 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+      else if (Months == '10' && Year == '2'){
+      var artis= parseInt(FirstNumber) * 7.89 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+      else if (Months == '11' && Year == '2'){
+      var artis= parseInt(FirstNumber) * 7.92 /100;
+       var result= parseInt(FirstNumber) + artis;
+     }
+      else if (Months == '12' && Year == '2'){
+      var artis= parseInt(FirstNumber) * 7.98 /100;
+       var result= parseInt(FirstNumber) + artis;
      }
        if (!isNaN(result)) {
            document.getElementById('hesapsonuc').value = result + ' TL';
