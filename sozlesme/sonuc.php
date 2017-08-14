a9c14f2a6dda200ec688e389d2f7572be94227d3
TuanaCelik <tc13007@my.bristol.ac.uk>
2017-08-14 15:24:32 +0300

    no message

--- sozlesme/sonuc.php
+++ sozlesme/sonuc.php
@@ -1,8 +1,8 @@
 <?php
 
+			header("Access-Control-Allow-Origin: *");
 			header('Content-disposition: attachment; filename=sozlesme.doc');
 			header('Content-type: application/doc');
-			header("Access-Control-Allow-Origin: *");
 			readfile('sozlesme.doc');
 			
 
